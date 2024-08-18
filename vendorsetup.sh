2be8da6f36f1aff93446da36efc5633481f556d6
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-08-18 19:31:22 +0800

    Fuck away SELinux

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Replace patched init.c in order to shut down selinux
+rm system/core/init/init.c
+cp device/xiaoxun/sl8521e_1h10ll_sw763/init.c system/core/init/init.c
+
 add_lunch_combo omni_sl8521e_2h10ll_watch_sw772-user
 add_lunch_combo omni_sl8521e_2h10ll_watch_sw772-userdebug
 add_lunch_combo omni_sl8521e_2h10ll_watch_sw772-eng
