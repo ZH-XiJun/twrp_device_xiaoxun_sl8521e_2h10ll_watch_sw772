ca59c5594193b74597deb63bc3420f1384910a76
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-08-19 16:46:06 +0800

    use diff patch instead of directly replacing file

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,9 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Replace patched init.c in order to shut down selinux
-rm system/core/init/init.c
-cp device/xiaoxun/sl8521e_2h10ll_watch_sw772/init.c system/core/init/init.c
+# Patch init.c in order to shut down selinux
+patch -Np1 < device/xiaoxun/sl8521e_2h10ll_watch_sw772/init.c.patch
 
 add_lunch_combo omni_sl8521e_2h10ll_watch_sw772-user
 add_lunch_combo omni_sl8521e_2h10ll_watch_sw772-userdebug
